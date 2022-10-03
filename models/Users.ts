105f51ce58a3177bac4596d16eb86efb0a9d627d
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2022-10-03 08:18:43 +0300

    lptp

--- models/Users.ts
+++ models/Users.ts
@@ -5,6 +5,7 @@ const UserSchema = new mongoosee.Schema(
         username: { type: String, required:true, unique:true },
         email: { type: String, required:true, unique:true },
         password: { type: String, required:true, unique:true },
+        favProducts:{type:Array},
         isAdmin: { type: Boolean, default: false }
     },
     {timestamps: true }
